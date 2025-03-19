5a570f9a661278cd9219cb60b00b109b675fc040
MaringantiYashwanth <yashwanthmaringanti@gmail.com>
2025-03-19 13:55:47 +0530

    Updated sql interview practice ibm db2 product analytics

--- sqlPractice.sql
+++ sqlPractice.sql
@@ -85,3 +85,24 @@ FROM emails e
 INNER JOIN texts t
 ON e.email_id = t.email_id
 WHERE signup_action = 'Confirmed' AND t.action_date=e.signup_date + INTERVAL '1 Day';
+
+-- IBM SQL Interview Question
+-- IBM DB2 Product Analytics
+WITH employee_queries AS (
+  SELECT 
+    e.employee_id,
+    COALESCE(COUNT(DISTINCT q.query_id), 0) AS unique_queries
+  FROM employees AS e
+  LEFT JOIN queries AS q
+    ON e.employee_id = q.employee_id
+      AND q.query_starttime >= '2023-07-01T00:00:00Z'
+      AND q.query_starttime < '2023-10-01T00:00:00Z'
+  GROUP BY e.employee_id
+)
+
+SELECT
+  unique_queries,
+  COUNT(employee_id) AS employee_count
+FROM employee_queries
+GROUP BY unique_queries
+ORDER BY unique_queries;
